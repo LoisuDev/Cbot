67555ec15a4617cd9a0c1742beacb51c3cd3624e
azusa-neko <76608613+azusa-neko@users.noreply.github.com>
2021-04-26 00:49:39 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-client.commands = new Discord.Collection();
+bot.commands = new Discord.Collection();
 const prefix = process.env.PREFIX;
 const fs = require('fs');
 const superagent = require('superagent');
@@ -17,7 +17,7 @@ const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
-	client.commands.set(command.name, command);
+	bot.commands.set(command.name, command);
 }
 
 
@@ -31,9 +31,9 @@ bot.on('message', async message => {
     const args = message.content.slice(prefix.length).trim().split(/ +/);
     const commandName = args.shift().toLowerCase();
 
-    if (!client.commands.has(commandName)) return;
+    if (!bot.commands.has(commandName)) return;
     
-    const command = client.commands.get(commandName);	
+    const command = bot.commands.get(commandName);	
 	
 	try {
 	command.execute(message, args);
