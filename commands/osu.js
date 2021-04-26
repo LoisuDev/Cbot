a507559aed56c0834e9cde3575351cff6ae7ed47
azusa-neko <76608613+azusa-neko@users.noreply.github.com>
2021-04-26 13:43:24 +0200

    Create osu.js

---
+++ commands/osu.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+
+module.exports = {
+  name: 'osu',
+  description: 'Le profil de joueurs osu du serveur ! (Contactez la fonda pour y apparaître)',
+  
+  async execute(message, args) {
+    if(!args.length) return message.channel.send('Veuillez préciser le joueur que vous souhaitez voir ! La liste des joueurs disponible se trouve avec %osu list');
+    if(args.length === 'list') return message.channel.send('Liste des joueurs disponibles :\n- Azuuusa\n-Nanamiiiiiiiii');
+    if(args.length === 'Azuuusa') return message.channel.send('**Azuuuusa**\n**Type de jeu** : osu!Relax\n**Niveau moyen de jeu (en étoiles)** : 4~6,5');
+    if(args.length === 'Nanamiiiiiiiii') return message.channel.send('**Nanamiiiiiiiii**\n**Type de jeu** : osu!AutoPilot\n**Niveau moyen de jeu (en étoiles)** : 1~3');
+  }
+}
