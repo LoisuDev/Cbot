72ba7c541353e4236db29cae876c44b387992dc2
azusa-neko <76608613+azusa-neko@users.noreply.github.com>
2021-04-26 16:41:46 +0200

    Create avatar.js

---
+++ commands/avatar.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+
+module.exports = {
+  name: 'avatar',
+  aliases: ['icon', 'pfp', 'pp'],
+  description: 'Montre ton avatar ou celui de la personne que tu as mentionnée.',
+  
+  async execute(bot, message, args) {
+    let member = message.mentions.users.first() || message.author;
+    let avatar = member.displayAvatarURL({size: 1024});
+    
+    let embed = new Discord.MessageEmbed()
+      .setTitle(`Avatar de : ${member.username}`)
+      .setImage(avatar)
+      .setFooter('avatar')
+      .setTimestamp()
+    
+    message.channel.send(embed);
+  }
+}
