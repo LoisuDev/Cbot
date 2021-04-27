a4576cfc6d8c12d5b6a3988488cf7438e9d66916
Loïs Libessart <lois.libessart@gmail.com>
2021-04-27 11:48:47 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -25,9 +25,11 @@ module.exports = {
     }
 
     data.push(`**Name:** ${command.name}`);
+    
 
     if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
     if (command.description) data.push(`**Description:** ${command.description}`);
+    if (command.usage) data.push(`**Utilisation**: ${command.usage}`)
     
     message.channel.send(data, { split: true });
     
