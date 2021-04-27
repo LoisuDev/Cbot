378823514b5ff0330a36152339d94aec6ade590a
Loïs Libessart <lois.libessart@gmail.com>
2021-04-27 12:04:52 +0200

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -4,6 +4,7 @@ module.exports = {
   name: 'userinfo',
   aliases : ['ui', 'whois'],
   description: 'Informations sur l\'utilisateur mentionné ou vous-même.',
+  usage: '%userinfo (<@utilisateur>)',
   
   async execute(message, args) {
     let member = message.mentions.users.first();
