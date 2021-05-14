4a9d1f8b02de2ad2b18b69b2e8cf05c333fa80d5
Loïs Libessart <lois.libessart@gmail.com>
2021-05-14 17:24:24 +0200

    Update schlong.js

--- commands/schlong.js
+++ commands/schlong.js
@@ -6,7 +6,7 @@ module.exports = {
   description: 'J\'AI MIS MA BITE SUR UN PINS',
   usage: '%schlong',
   
-  async execute(message, args) => {
+  async execute(message, args) {
     return message.channel.send('https://www.youtube.com/watch?v=_Oz6nvkLyvY');
   }
 }
