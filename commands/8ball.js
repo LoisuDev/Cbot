e58233a0ad5c409cb7a69dec00c3e271118ca2b8
Loïs Libessart <lois.libessart@gmail.com>
2021-05-18 15:31:51 +0200

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 module.exports = {
   name: "8ball",
   aliases: ['8b'],
-  description: "8ball !",
+  description: "Pose une question et le bot te répondra par oui, non ou peut-être.",
   usage: '%8ball <question>',
   execute(message, args) {
         if (!args[1]) return message.reply("Entrez une question !")
