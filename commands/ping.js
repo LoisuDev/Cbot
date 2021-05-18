5e621a7d07a7813797652df9b12ccad8ee4ede3e
Loïs Libessart <lois.libessart@gmail.com>
2021-05-18 15:36:12 +0200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 module.exports = {
   name: 'ping',
-  description: 'Ping!',
+  description: 'pong.',
   execute(message, args) {
      var ping = Date.now() - message.createdTimestamp + " ms";
         message.channel.send("Pong !\n`" + `${Date.now() - message.createdTimestamp}` + " ms`");
