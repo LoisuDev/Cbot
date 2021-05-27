624e1e74d7e6183724a43df43e56f7bca004691b
Loïs Libessart <lois.libessart@gmail.com>
2021-05-27 11:37:20 +0200

    Create grandoral.js

---
+++ commands/grandoral.js
@@ -0,0 +1,12 @@
+/* const Discord = require('discord.js');
+
+module.exports = {
+  name: 'grandoral',
+  description: 'Commande destinée à aider Loïs dans ses révisions pour le grand oral.',
+  usage: '%grandoral <spécialité>%[partie]',
+  
+  async execute(message, args) {
+    let argument = args.join(' ').split('%');
+    
+  }
+}
