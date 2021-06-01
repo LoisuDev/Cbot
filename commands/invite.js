f2edba9bd0a678a1f5425e6fe5433a4d5330c3bd
Loïs Libessart <lois.libessart@gmail.com>
2021-06-01 19:40:56 +0200

    Create invite.js

---
+++ commands/invite.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+
+module.exports = {
+  name: 'invite',
+  description: 'Returns a link to invite CabaneBot in your server.',
+  usage: '%invite',
+  
+  async execute (message, args) {
+    return message.channel.send('https://discord.com/api/oauth2/authorize?client_id=828715499894079488&permissions=8&scope=bot')
+  }
+}
