e46db02ba70de59ae96ca043b39f9527439b0e1a
Loïs Libessart <lois.libessart@gmail.com>
2021-07-01 20:30:37 +0200

    Update nom.js

--- commands/nom.js
+++ commands/nom.js
@@ -4,13 +4,13 @@ const superagent = require('superagent');
 module.exports = {
   name: 'nom',
   aliases: ['manger'],
-  description: 'Vous avez faim, à ce que je vois',
+  description: 'I can tell, you\'re hungry, don\'t you?',
   
   async execute(message, args) {
     let { body } = await superagent.get(`https://shiro.gg/api/images/nom`);
     let hug_embed = new Discord.MessageEmbed()
       .setColor('e410d3')
-      .setTitle(`${message.author.username} a faim !`)
+      .setTitle(`${message.author.username} is hungry!`)
       .setImage(body.url)
       .setFooter('nom')
       .setTimestamp()
