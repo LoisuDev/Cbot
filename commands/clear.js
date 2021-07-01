c0e1a7e4267c378376dd6d4555399e1ca7edd41c
Loïs Libessart <lois.libessart@gmail.com>
2021-07-01 19:57:37 +0200

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -2,17 +2,17 @@ const Discord = require('discord.js');
 
 module.exports = {
   name: "clear",
-  description: "Supprime un montant de messages désiré",
-  usage: '%clear <nombre de messages à supprimer>',
-  permissions: ['Gérer les messages'],
+  description: "Clears the number of messages you provided.",
+  usage: '%clear <number of messages to delete>',
+  permissions: ['Manage Messages'],
   execute(message, args) {
         if (!message.member.hasPermission("MANAGE_MESSAGES")) 
-        return message.channel.send("Senpai, vous n'avez pas le droit de faire ça >.< ♥")
+        return message.channel.send("Senpai, you can't do that! >.< ♥")
         if (!args[0])
-        return message.channel.send("Senpai, voici comment on supprime des messages >.< ♥ : ``<prefix>clear <nbr de msg à clear>``");
+        return message.channel.send("Invalid arguments provided.");
 
         message.channel.bulkDelete(args[0]).then(() => {
-        message.channel.send(`Senpai, j'ai supprimé **__${args[0]} messages__** rien que pour vous. >///< ♥`);
+        message.channel.send(`Senpai, I deleted **__${args[0]} messages__** just for you. >///< ♥`);
         })
         message.delete();
   }
