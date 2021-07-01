391272f68faf2fcb7d469feb69f3706e4e296f7d
Loïs Libessart <lois.libessart@gmail.com>
2021-07-01 20:38:37 +0200

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -3,8 +3,6 @@ const Discord = require('discord.js');
 module.exports = {
   name: 'say',
   aliases: ['dis'],
-  description: 'Vous me faites dire ce que vous voulez (bon pas de trucs méchants hein !)',
-  usage: '%say <ce que vous voulez me faire dire>',
   
   async execute(message, args) {
     let msg = args.join(' ');
