2c0b4fb2f8b68099b15e3966c20d177d086e521a
Loïs Libessart <lois.libessart@gmail.com>
2021-08-02 22:05:22 +0200

    Update tickle.js

--- commands/tickle.js
+++ commands/tickle.js
@@ -8,8 +8,8 @@ module.exports = {
   async execute(message, args) {
     let member = message.mentions.users.first();
     let { body } = await superagent.get(`https://shiro.gg/api/images/tickle`);
-    if(!member) return message.channel.send(body.url);
     if(message.author.id === "573426170289455154") return message.channel.send('https://media1.tenor.com/images/3fc942141e181ef927813f0a5a679193/tenor.gif');
+    if(!member) return message.channel.send(body.url);
     if(member.id === "655605141046820884") return message.channel.send("Sorry, for some reason you can\'t do that...")
     
     
