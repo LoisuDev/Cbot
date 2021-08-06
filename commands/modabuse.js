31c30a4435780275bcfea09c1cdd9c73c1cd4ad5
Loïs Libessart <lois.libessart@gmail.com>
2021-08-06 18:19:13 +0200

    Update modabuse.js

--- commands/modabuse.js
+++ commands/modabuse.js
@@ -11,7 +11,7 @@ module.exports = {
     let button = new disbut.MessageButton()
       .setStyle('url')
       .setLabel('Watch our rights')
-      .setURL('shorturl.at/nrJV1')
+      .setURL('https://shorturl.at/nrJV1')
     return message.channel.send('Watch admins/mods rights by clicking the button below.\nhttps://media1.tenor.com/images/78d61511dfa9d3e174dd9978528f7ca3/tenor.gif?itemid=17248464', button)
   }
 }
