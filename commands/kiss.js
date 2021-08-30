ce3848be7596a7f810965f1472d030dc5a54fa20
Loïs Libessart <lois.libessart@gmail.com>
2021-08-30 04:09:11 +0200

    Update kiss.js

--- commands/kiss.js
+++ commands/kiss.js
@@ -5,7 +5,7 @@ module.exports = {
   name: 'kiss',
   aliases: ['bisou'],
   
-  async execute(message, args) {
+  async execute(bot, message, args) {
     let member = message.mentions.members.first();
     let { body } = await superagent.get(`https://nekos.life/api/kiss`);
     if(!member) return message.channel.send(body.url);
