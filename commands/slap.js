c13dbb721a38343e928a4792230ac52007a88a29
Loïs Libessart <lois.libessart@gmail.com>
2021-08-30 04:13:31 +0200

    Update slap.js

--- commands/slap.js
+++ commands/slap.js
@@ -6,7 +6,7 @@ module.exports = {
   aliases: ['taper'],
   description: 'Meanie!',
   
-  async execute(message, args) {
+  async execute(bot, message, args) {
     let member = message.mentions.members.first();
     let { body } = await superagent.get(`https://shiro.gg/api/images/slap`);
     if(!member) return message.channel.send(body.url);
