1a6d86081a2688253580f02c1636128a0404a66b
Loïs Libessart <lois.libessart@gmail.com>
2021-08-30 04:33:08 +0200

    Update cat.js

--- commands/cat.js
+++ commands/cat.js
@@ -8,12 +8,12 @@ module.exports = {
   usage: "%cat",
   
   async execute (bot, message, args) {
-    let { body } = await superagent.get(`https://aws.random.cat/meow`)
+    let { body } = await superagent.get(`https://api.thecatapi.com/v1/images/search`)
     
     let embed = new Discord.MessageEmbed()
       .setColor('e410d3')
       .setTitle('Meow!')
-      .setImage(body.file)
+      .setImage(body.url)
       .setFooter('cat')
       .setTimestamp()
     return message.channel.send(embed);
