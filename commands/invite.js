b602f77435ad1d1bf173e29f3bc3ffc61bcad303
Loïs Libessart <lois.libessart@gmail.com>
2021-08-30 04:08:50 +0200

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -5,7 +5,7 @@ module.exports = {
   description: 'Returns a link to invite Sotoka in your server.',
   usage: '%invite',
   
-  async execute (message, args) {
+  async execute (bot, message, args) {
     return message.channel.send('https://discord.com/api/oauth2/authorize?client_id=828715499894079488&permissions=8&scope=bot')
   }
 }
